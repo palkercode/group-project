8a67abbeb15d01d28a5c38320146efaeb2f9c8d3
palkercode <huypauka@gmail.com>
2025-01-21 11:55:36 +0500

    da

--- src/main/java/database/PostgreSQL.java
+++ src/main/java/database/PostgreSQL.java
@@ -2,10 +2,24 @@ package database;
 
 import java.sql.Connection;
 
-public class PostgreSQL implements Connectable{
+public class PostgreSQL implements Connectable {
+    private String host;
+    private String username;
+    private String password;
+    private String dbName;
+
+    private Connection connection;
+
+    public PostgreSQL(String host, String username, String password, String dbName) {
+        this.host = host;
+        this.username = username;
+        this.password = password;
+        this.dbName = dbName;
+    }
+
     @Override
     public Connection getConnection() {
-        return null;
+        // TODO: da
     }
 
     @Override
